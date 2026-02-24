ec7c1046e3bae689f2c798c88cdb9d9296e1a5a6
Alex <atwitty24@gmail.com>
2026-02-24 04:11:22 -0500

    Fix Ghost spawn position

--- Assets/ch3URP/SceneController.cs
+++ Assets/ch3URP/SceneController.cs
@@ -7,7 +7,7 @@ public class SceneController : MonoBehaviour {
     void Start() {
         if (!hasSpawnedInitialEnemy) {
             GameObject enemy = Instantiate(enemyPrefab);
-            enemy.transform.position = new Vector3(0, 1, 0);
+            enemy.transform.position = new Vector3(0, (float)1.2, 0);
             float angle = Random.Range(0, 360);
             enemy.transform.Rotate(0, angle, 0);
             hasSpawnedInitialEnemy = true;
